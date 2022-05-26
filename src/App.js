6c7f938833edeeabae22ce61730aa449d3332035
JovitoAddo <jovitoaddo@gmail.com>
2022-05-26 21:25:58 +0700

    commit for netlify, still under construction

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Navbar from "./Pages/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Footer from "./Pages/Footer";
-import Contents from './Pages/Contents'
+import Contents from './Pages/Contents';
+import UnConst from './Pages/UnConst';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Index />}>
-        <Route index element={<Contents />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
+        <Route path="/" element={<UnConst />}>
+        <Route index element={<UnConst />} />
+          <Route path="home" element={<UnConst />} />
+          <Route path="about" element={<UnConst />} />
         </Route>
       </Routes>
     </div>
---
+++ src/Pages/UnConst.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UnConst() {
+  return (
+    <h1>UNDER CONSTRUCTION</h1>
+  )
+}
+
+export default UnConst
