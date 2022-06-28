c1de3d5e6172c905b04607cb11cf17ab6a33bc9e
JovitoAddo <jovitoaddo@gmail.com>
2022-06-28 18:31:39 +0700

    fixin for firefox

--- src/Pages/Certificate.js
+++ src/Pages/Certificate.js
@@ -2,12 +2,15 @@ import React from "react";
 import certificate from "../Assets/certificate.png";
 
 function Certificate() {
+    function HandleClick(){
+        window.open("https://network.impactbyte.com/certificate/jovito-addo", "_blank")
+    }
   return (
     <div id="certificate" className="grid justify-items-center bg-cust4 pb-3">
       <p className=" pb-3 text-5xl text-cust2">Certificate</p>
-      <a href="https://network.impactbyte.com/certificate/jovito-addo">
+      <div onClick={HandleClick} className='hover:cursor-pointer'>
         <img src={certificate} className="h-5/6 hover:opacity-60 rounded" />
-      </a>
+      </div>
     </div>
   );
 }
