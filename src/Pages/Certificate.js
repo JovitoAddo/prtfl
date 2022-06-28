a90d5eb80c10c2041f35664f8889a54ec127c44b
JovitoAddo <jovitoaddo@gmail.com>
2022-06-28 18:45:16 +0700

    fixing for firefox part2

--- src/Pages/Certificate.js
+++ src/Pages/Certificate.js
@@ -6,9 +6,9 @@ function Certificate() {
         window.open("https://network.impactbyte.com/certificate/jovito-addo", "_blank")
     }
   return (
-    <div id="certificate" className="grid justify-items-center bg-cust4 pb-3">
+    <div id="certificate" className="grid justify-items-center bg-cust4 pb-3 w-full">
       <p className=" pb-3 text-5xl text-cust2">Certificate</p>
-      <div onClick={HandleClick} className='hover:cursor-pointer'>
+      <div onClick={HandleClick} className='hover:cursor-pointer flex justify-content-center'>
         <img src={certificate} className="h-5/6 hover:opacity-60 rounded" />
       </div>
     </div>
