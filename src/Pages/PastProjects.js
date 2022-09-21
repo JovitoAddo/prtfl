2dc804756a488567aaee15f83b19f5f9cadf8db7
JovitoAddo <jovitoaddo@gmail.com>
2022-09-21 20:01:16 +0700

    fix bug

--- src/Pages/PastProjects.js
+++ src/Pages/PastProjects.js
@@ -238,7 +238,7 @@ function PastProjects() {
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-bounce"
+                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-spin"
                           width="35"
                           height="35"
                           viewBox="0 0 24 24"
@@ -333,15 +333,15 @@ function PastProjects() {
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-bounce"
+                          className="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-spin"
                           width="35"
                           height="35"
                           viewBox="0 0 24 24"
-                          stroke-width="1.5"
+                          strokeWidth="1.5"
                           stroke="#ffffff"
                           fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         >
                           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                           <circle cx="12" cy="12" r="9" />
@@ -420,7 +420,7 @@ function PastProjects() {
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-bounce"
+                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-spin"
                           width="35"
                           height="35"
                           viewBox="0 0 24 24"
@@ -510,7 +510,7 @@ function PastProjects() {
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-bounce"
+                          class="icon icon-tabler icon-tabler-circle-x fill-cust4 hover:fill-cust3 hover:animate-spin"
                           width="35"
                           height="35"
                           viewBox="0 0 24 24"
